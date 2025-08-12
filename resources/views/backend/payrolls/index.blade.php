3d396afb8f11de1fab61595dbedacd0ad047e7af
Mohamed Samy <mohamedsabaawy@Hotmail.com>
2025-08-13 01:27:13 +0300

    rr

--- resources/views/backend/payrolls/index.blade.php
+++ resources/views/backend/payrolls/index.blade.php
@@ -15,43 +15,35 @@
                             <div class="card-header d-flex justify-content-between align-items-center">
                                 <h4 class="card-title m-0">
                                     <a class="d-block" data-toggle="collapse" href="#collapseOne">
-                                        Payroll List
+                                        {{ __('dashboard.payroll_list') }}
                                     </a>
                                 </h4>
                                 <div class="ml-auto">
-                                    <a href="{{ url('admin/payroll/add') }}" class="btn btn-primary text-white">Create
-                                        Payroll</a>
+                                    <a href="{{ url('admin/payroll/add') }}" class="btn btn-primary text-white">{{ __('dashboard.create_payroll') }}</a>
                                 </div>
                             </div>
                             <div id="collapseOne" class="collapse " data-parent="#accordion">
                                 <div class="card-body">
 
-                                    ⚠️ <strong>Important:</strong> Before creating payroll, make sure your company policy is
-                                    configured correctly.
-                                    special attention to <strong>working days</strong> and <strong>official
-                                        holidays</strong>,
-                                    as they directly affect attendance deductions.<br><br>
+                                    ⚠️ <strong>{{ __('dashboard.important') }}</strong>
+                                    {{ __('dashboard.before_creating_payroll') }}
+                                    <strong>{{ __('dashboard.working_days') }}</strong>
+                                    {{ __('dashboard.and') }}
+                                    <strong>{{ __('dashboard.official_holidays') }}</strong>,
+                                    {{ __('dashboard.as_they_affect_deductions') }}<br><br>
 
-                                    <strong>Clarification of the Payroll :</strong><br><br>
+                                    <strong>{{ __('dashboard.clarification_of_payroll') }}</strong><br><br>
 
-                                    • <strong>Bonus</strong> – Calculated based on the company's bonus policy for extra
-                                    hours in company policy.<br>
+                                    • <strong>{{ __('dashboard.bonus') }}</strong> – {{ __('dashboard.bonus_explanation') }}<br>
 
-                                    • <strong>Deductions</strong> – Includes manual deductions, penalties, or extra vacation
-                                    days taken beyond the balance.<br>
+                                    • <strong>{{ __('dashboard.deductions') }}</strong> – {{ __('dashboard.deductions_explanation') }}<br>
 
-                                    • <strong>Taxes / Insurance</strong> – Automatically deducted based on the configured
-                                    percentages locate in the tax and insurance.<br>
+                                    • <strong>{{ __('dashboard.taxes_insurance') }}</strong> – {{ __('dashboard.taxes_insurance_explanation') }}<br>
 
-                                    • <strong>Total Vacation Balance</strong> – Determined by the company’s policy (e.g., 21
-                                    or 30 days per year).<br>
+                                    • <strong>{{ __('dashboard.total_vacation_balance') }}</strong> – {{ __('dashboard.total_vacation_balance_explanation') }}<br>
 
-
-
-
-                                    • <strong>Net Pay</strong> – Calculated as:<br>
-                                    <code>Net Pay = Basic Salary - (Taxes + Insurance + Deductions + Attendance Deductions)
-                                        + Bonus</code>
+                                    • <strong>{{ __('dashboard.net_pay') }}</strong> – {{ __('dashboard.net_pay_explanation') }}<br>
+                                    <code>{{ __('dashboard.net_pay_formula') }}</code>
                                 </div>
 
                             </div>
@@ -69,7 +61,7 @@
                 <div class="row">
 
                     <div class="form-group col-md-2 col-sm-6">
-                        <label>Employee Name</label>
+                        <label>{{ __('dashboard.employee_name') }}</label>
                         <input type="text" value="{{ Request::get('name') }}" name="name" class="form-control"
                             placeholder="Enter Name"
                             style="background-color: rgba(255, 255, 255, 0.5); color: black; border: 1px solid rgba(255, 255, 255, 0.8);">
@@ -80,25 +72,25 @@
 
 
                     <div class="form-group col-md-2 col-sm-6">
-                        <label>Payroll Type</label>
+                        <label>{{ __('dashboard.payroll_type') }}</label>
                         <select name="payroll_type" class="form-control"
                             style="background-color: rgba(255, 255, 255, 0.5); border: 1px solid rgba(255, 255, 255, 0.8);">
-                            <option value="">Select Payroll Type</option>
-                            <option value="monthly" {{ Request::get('payroll_type') == 'monthly' ? 'selected' : '' }}>Monthly
+                            <option value="">{{ __('dashboard.select_payroll_type') }}</option>
+                            <option value="monthly" {{ Request::get('payroll_type') == 'monthly' ? 'selected' : '' }}>{{ __('dashboard.monthly') }}
                             </option>
-                            <option value="weekly" {{ Request::get('payroll_type') == 'weekly' ? 'selected' : '' }}>Weekly
+                            <option value="weekly" {{ Request::get('payroll_type') == 'weekly' ? 'selected' : '' }}>{{ __('dashboard.weekly') }}
                             </option>
-                            <option value="daily" {{ Request::get('payroll_type') == 'daily' ? 'selected' : '' }}>Daily
+                            <option value="daily" {{ Request::get('payroll_type') == 'daily' ? 'selected' : '' }}>{{ __('dashboard.daily') }}
                             </option>
                         </select>
                     </div>
 
 
                     <div class="form-group col-md-2 col-sm-6">
-                        <label>Month</label>
+                        <label>{{ __('dashboard.month') }}</label>
                         <select name="month" class="form-control"
                             style="background-color: rgba(255, 255, 255, 0.5); border: 1px solid rgba(255, 255, 255, 0.8);">
-                            <option value="">Select Month</option>
+                            <option value="">{{ __('dashboard.select_month') }}</option>
                             @for ($i = 1; $i <= 12; $i++)
                                 <option value="{{ $i }}" {{ Request::get('month') == $i ? 'selected' : '' }}>
                                     {{ date('F', mktime(0, 0, 0, $i, 1)) }}</option>
@@ -108,10 +100,10 @@
 
 
                     <div class="form-group col-md-2 col-sm-6">
-                        <label>Year</label>
+                        <label>{{ __('dashboard.year') }}</label>
                         <select name="year" class="form-control"
                             style="background-color: rgba(255, 255, 255, 0.5); border: 1px solid rgba(255, 255, 255, 0.8);">
-                            <option value="">Select Year</option>
+                            <option value="">{{ __('dashboard.select_year') }}</option>
                             @php
                                 $currentYear = date('Y');
                                 $endYear = $currentYear + 6; // Show 6 years into the future
@@ -142,10 +134,10 @@
         <div>
             <a class="btn btn-success rounded-pill"
                 href="{{ url(path: 'admin/payroll_export?name=' . Request::get('name') . '&month=' . Request::get('month') . '&year=' . Request::get('year')  . '&payroll_type=' . Request::get('payroll_type')) }}">
-                <i class="fas fa-file-excel"></i> Excel
+                <i class="fas fa-file-excel"></i> {{ __('dashboard.excel') }}
             </a>
             <a href="{{ route('payrolls.exportPdf', Request::all()) }}" class="btn btn-danger rounded-pill ms-2">
-                <i class="fas fa-file-pdf"></i> PDF
+                <i class="fas fa-file-pdf"></i> {{ __('dashboard.pdf') }}
             </a>
         </div>
     </div>
@@ -164,9 +156,9 @@
         <div class="card"
             style="background-color: rgba(255, 255, 255, 0.7); border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
             <div class="card-header d-flex justify-content-between align-items-center flex-wrap">
-                <h3 class="card-title mb-2 mb-md-0">Payroll List</h3>
+                <h3 class="card-title mb-2 mb-md-0">{{ __('dashboard.payroll_list') }}</h3>
                 <div class="ml-auto">
-                    <button class="btn btn-danger" id="deleteSelected">Delete Selection</button>
+                    <button class="btn btn-danger" id="deleteSelected">{{ __('dashboard.delete_selection') }}</button>
                 </div>
             </div>
 
@@ -175,19 +167,19 @@
                     <thead>
                         <tr>
                             <th><input type="checkbox" id="selectAll"></th>
-                            <th>Employee ID</th>
-                            <th>Employee Name</th>
-                            <th>Basic Salary</th>
-                            <th>Bounas</th>
-                            <th>Deductions</th>
-                            <th>ِAttendance Deduction</th>
-                            <th>Taxes/Inscurance</th>
-                            <th>Vacation Balance</th>
-                            <th>Net Pay</th>
-                            <th>Payroll Type</th>
-                            <th>Pay Date</th>
-                            <th>Month</th>
-                            <th>Action</th>
+                            <th>{{ __('dashboard.employee_id') }}</th>
+                            <th>{{ __('dashboard.employee_name') }}</th>
+                            <th>{{ __('dashboard.basic_salary') }}</th>
+                            <th>{{ __('dashboard.bounas') }}</th>
+                            <th>{{ __('dashboard.deductions') }}</th>
+                            <th>{{ __('dashboard.attendance_deduction') }}</th>
+                            <th>{{ __('dashboard.taxes_inscurance') }}</th>
+                            <th>{{ __('dashboard.vacation_balance') }}</th>
+                            <th>{{ __('dashboard.net_pay') }}</th>
+                            <th>{{ __('dashboard.payroll_type') }}</th>
+                            <th>{{ __('dashboard.pay_date') }}</th>
+                            <th>{{ __('dashboard.month') }}</th>
+                            <th>{{ __('dashboard.action') }}</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -204,7 +196,7 @@
 
                                 <td>
                                     @if ($value->payroll_type == 'monthly')
-                                        {{ $value->rest_vacancy }} day
+                                        {{ $value->rest_vacancy }} {{ __('dashboard.day') }}
                                     @else
                                         0
                                     @endif
