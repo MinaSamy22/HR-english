2d9c3cd089e9d3ca7b6c25b197d88cf9565674dc
Mohamed Samy <mohamedsabaawy@Hotmail.com>
2025-09-19 22:22:23 +0300

    Update checkDeviceMiddleware.php

--- app/Http/Middleware/checkDeviceMiddleware.php
+++ app/Http/Middleware/checkDeviceMiddleware.php
@@ -4,8 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
+
+use function included\sendResponse;
 
 class checkDeviceMiddleware
 {
@@ -16,14 +17,19 @@ class checkDeviceMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // if ($request->user() && $request->user()->macaddress) {
-        //     $deviceIdFromRequest = $request->header('Device-ID');
+        if ($request->user()) {
+            $deviceIdFromRequest = $request->header('deviceserial');
 
-        //     if ($deviceIdFromRequest !== $request->user()->macaddress) {
-        //         return response()->json(['message' => 'Unauthorized device'], 401);
-        //     }
-        // }
-        Log::info("message",$request->header());
+            if (empty($request->user()->macaddress)) {
+                // Save the first device serial as the macaddress
+                $request->user()->update([
+                    'macaddress' => $deviceIdFromRequest,
+                ]);
+            } elseif ($deviceIdFromRequest !== $request->user()->macaddress) {
+                return sendResponse([],'Unauthorized device', 0);
+            }
+        }
+
         return $next($request);
     }
 }
