983de55f28e66a51ca12b32a1f656f176059fb49
minasamy43 <mina.samy@gold-era.com>
2026-08-10 16:16:10 +0300

    .

--- resources/views/backend/employees/list.blade.php
+++ resources/views/backend/employees/list.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper"
-        style="background-image: url('{{ asset('/dist/img/generall.png') }}'); background-size: cover; background-position: center;">
+    <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container-fluid">
